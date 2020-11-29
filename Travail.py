3e2ad9bab73d5ecc479c96f1af5b62dc4e994208
Joseph-Makhol <74927758+Joseph-Makhol@users.noreply.github.com>
2020-11-28 22:46:43 -0500

    Add files via upload

---
+++ Travail.py
@@ -0,0 +1,34 @@
+#Ceci est un code qui deamnde une information spécifique à l'utilisateur
+#Par: Joseph Makhol
+x = 2
+y = 2.0
+
+print("Entrer un nombre paire qui se divise par 2")
+
+user_input = input()
+user_input = int(user_input)
+
+result = int(user_input/x)
+
+print("la reponce est", result)
+
+print("Maintenant, entrer un nomre impaire que tu veux diviser en 2")
+user_input = input()
+user_input = float(user_input)
+
+result = user_input/y
+print("la reponce est", result)
+
+print("Finalement, entrer un nombre decimal que tu ve diviser en 2")
+
+user_input = input()
+user_input = float(user_input)
+
+result = user_input/y
+print("la reponce est", result)
+
+
+
+
+
+
