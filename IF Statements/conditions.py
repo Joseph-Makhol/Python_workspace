733e45c8eb069516bce651f358904fc2d3bdfc8b
unknown <cecce@FRCLOC203-67410.ecolecatholique.ca>
2020-12-02 14:25:41 -0500

    conditions

---
+++ IF Statements/conditions.py
@@ -0,0 +1,18 @@
+print("Quelle est ton age?")   
+
+age = float(input())
+
+
+if age <= 30:
+    print("Tu es jeune!!!")
+
+elif age <= 40:
+    print("You are getting there")
+
+elif age <= 100: 
+    print("Vous êtes vieux")   
+
+else: 
+    print("Je n'ai pas compris", choix)    
+    
+print("Merci d'avoir participer")
