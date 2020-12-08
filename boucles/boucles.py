fb51706f2b7ff07855728f43a74fea68f4d0a84d
Joseph-Makhol <74927758+Joseph-Makhol@users.noreply.github.com>
2020-12-08 16:13:20 -0500

    Add files via upload

---
+++ boucles/boucles.py
@@ -0,0 +1,23 @@
+# une programme while et for
+# par Joseph Makhol   makjos02@ecolecatholique.ca
+"""
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+"""
+
+x = 1
+while x < 6:
+  print(x)
+  x += 1
+
+print("parmi les nombres, choisie un que tu aimerais multiplier par 2")
+
+user_input = input()
+print(x*2)
+print("merci de jouer")
+
+  
+  
+ 
