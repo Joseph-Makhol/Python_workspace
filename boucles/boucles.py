71fc90191851d8c7bef793d887e598cd854ec58a
unknown <cecce@FRCLOC203-67410.ecolecatholique.ca>
2020-12-09 10:45:49 -0500

    boucles

--- boucles/boucles.py
+++ boucles/boucles.py
@@ -1,22 +1,59 @@
 # une programme while et for
 # par Joseph Makhol   makjos02@ecolecatholique.ca
-"""
-i = 1
-while i < 6:
-  print(i)
-  i += 1
-"""
 
 x = 1
-while x < 6:
+while x < 11:
   print(x)
   x += 1
 
 print("parmi les nombres, choisie un que tu aimerais multiplier par 2")
 
-user_input = input()
-print(x*2)
-print("merci de jouer")
+user_input = float(input())
+if user_input < 11:
+  print(user_input*2)
+else:
+  print("Erreur, le numéro que vous avez choisi n'était pas nommer par le programme")
+
+print("Choisi un nombre que tu aimerais diviser en 2")
+
+for i in range(11):
+  print(i)
+choix = float(input())  
+if choix < 11:
+  print(choix/2)
+else:
+  print("Erreur, le numéro que vous avez choisi n'était pas nommer par le programme")
+
+
+while True:
+  print("Veux tu continuer?")
+
+  print("Si non, dit non. Si oui, dit oui")
+  end = input().lower()
+  if end == "non":
+    break
+  else:  
+    print("CALCULE MENTAL... 5+5 est...")
+    ansr = int(input())
+    if ansr == 10:
+      print("YAY!!!")
+    else:
+      print("so close:(")
+    break
+  
+print("Merci d'avoir jouer!!!")
+
+
+
+
+
+
+
+
+
+
+
+
 
   
   
